ae03e8c0dc244a62cae577e54f0acf3490a91510
Alexander Bruy <alexander.bruy@gmail.com>
2018-01-17 19:10:03 +0200

    prepare infrastructure for translations (work in progress)

---
+++ i18n/processing_gnm_uk.ts
@@ -0,0 +1,283 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>ConnectivityTreePoint</name>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="62"/>
+        <source>Connectivity tree (point to point)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="65"/>
+        <source>Network analysis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="74"/>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="77"/>
+        <source>GFID of the start node</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="81"/>
+        <source>GFID of the end node</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="86"/>
+        <source>Comma-separated GFIDs of the blocked nodes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="91"/>
+        <source>Connectivity tree</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="101"/>
+        <source>Start and end points should be different.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="107"/>
+        <source>Start point can not be blocked.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="110"/>
+        <source>End point can not be blocked.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="118"/>
+        <source>Can not open generic network dataset.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="137"/>
+        <source>Error occured during connectivity tree calculation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../connectivitytreepoint.py" line="140"/>
+        <source>Start node has no connections with other nodes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CreateNetwork</name>
+    <message>
+        <location filename="../createnetwork.py" line="59"/>
+        <source>Create network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="62"/>
+        <source>Network management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="71"/>
+        <source>Layer to add to the network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="73"/>
+        <source>Topology tolerance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="80"/>
+        <source>Network CRS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="83"/>
+        <source>Network name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="85"/>
+        <source>Network description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="88"/>
+        <source>Network rules</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="93"/>
+        <source>Output directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="102"/>
+        <source>Network name can not be empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="107"/>
+        <source>Can not initialize GNM driver.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="120"/>
+        <source>Can not initialize network dataset.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="124"/>
+        <source>Can not initialize generic network dataset.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="134"/>
+        <source>Can not open dataset {}.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="139"/>
+        <source>Can not fetch layer 0 from the dataset {}.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="145"/>
+        <source>Could not import layer 0 from the dataset {} into the network.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="164"/>
+        <source>Can not create rule &apos;{}&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="168"/>
+        <source>No point layers were imported. We will not be able to build network topology.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="171"/>
+        <source>No line layers were imported. We will not be able to build network topology.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../createnetwork.py" line="179"/>
+        <source>Can not build network topology.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GnmProvider</name>
+    <message>
+        <location filename="../gnmProvider.py" line="64"/>
+        <source>Activate</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RemoveNetwork</name>
+    <message>
+        <location filename="../removenetwork.py" line="43"/>
+        <source>Remove network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../removenetwork.py" line="46"/>
+        <source>Network management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../removenetwork.py" line="55"/>
+        <source>Network to remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../removenetwork.py" line="58"/>
+        <source>Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../removenetwork.py" line="64"/>
+        <source>Can not initialize GNM driver.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ShortestPathPointToPoint</name>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="63"/>
+        <source>Shortest paths (point to point)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="66"/>
+        <source>Network analysis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="75"/>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="78"/>
+        <source>Number of paths to calculate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="82"/>
+        <source>GFID of the start node</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="86"/>
+        <source>GFID of the end node</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="90"/>
+        <source>Comma-separated GFIDs of the blocked nodes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="95"/>
+        <source>Shortest path(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="105"/>
+        <source>Start and end points should be different.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="111"/>
+        <source>Start point can not be blocked.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="114"/>
+        <source>End point can not be blocked.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="122"/>
+        <source>Can not open generic network dataset.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="143"/>
+        <source>Error occured during shortest path calculation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../shortestpathpointtopoint.py" line="146"/>
+        <source>There is no path from the start point to the end point.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
